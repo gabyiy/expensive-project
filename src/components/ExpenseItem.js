c7e6aee5644f5490fa4bac377a60e0bcac8fb354
Nita <gabyiy2000@hotmail.com>
2021-07-20 16:05:34 +0200

    a

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,18 @@
 import "./ExpenseItem.css"; //asa specificam ca dorim sa folosim css items aici
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   return (
     //iar aici adaugam clasele care le avem disponibile din css
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
+
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <div className="expense-item__price">${props.amount}</div>
-    </div>
+    </Card>
   );
 }
 
